4db5474602a63cb3b1e54ec195b4891e969ed106
Rafaela119575 <rafaela119575@mcpsva.org>
2026-08-27 10:42:16 -0400

    hi

---
+++ Urquizo_Rafaela_interview.zip.py
@@ -0,0 +1,24 @@
+'''
+
+Filename: interview.py
+Author: Urquizo, Rafaela
+Date: 08/25/36
+Instructor: Burgess
+'''
+
+print('Welcome to Osbourn High School! Thank you for taking the time to interview with us')
+print('today. I am your automated interviewer, and I will be guiding you through a quick 5-')
+print('question personality assessment. We want to get to know the real you, so please relax')
+print('and answer honestly. Lets begin!')
+print()
+x_exp=input('What was your previous experience?\n')
+print(x_exp)
+x_prog=input('How familiar are you with this program?\n')
+print(x_prog)
+x_sit = input('What would you do in a situation like this?\n')
+print(x_sit)
+x_years=input('How many years have you been doing this?\n')
+print(x_years)
+x_proj=input('Have you worked on any large projects before?\n')
+print(x_proj)
+print('Thats all, thank you, we gonna call you')
